f3a10839c3cd64e7d92387a2ef419f465f5e4d55
muzuka <seanbrown7935@gmail.com>
2017-01-22 00:06:45 -0700

    Created Platform class.

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Platform {
+
+	public int height { get; set; }
+	public int width { get; set; }
+	public GameObject[] tiles { get; set; }
+	public GameObject top { get; set; }
+	public GameObject left { get; set; }
+	public GameObject right { get; set; }
+
+	public Platform (int h)
+	{
+		height = h;
+		tiles = new GameObject[6];
+		top = null;
+		left = null;
+		right = null;
+
+		//top.GetComponent<AudioSource>().clip = Resources.Load<GameObject>("Prefabs/Notes/Note" + height).GetComponent<AudioSource>().clip;
+	}
+}
