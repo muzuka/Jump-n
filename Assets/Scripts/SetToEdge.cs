b453e931e584ab15a385bcc50ca717b7241938b5
muzuka <seanbrown7935@gmail.com>
2017-01-21 19:34:06 -0700

    Snaps empty to both sides of screen.

--- Assets/Scripts/SetToEdge.cs
+++ Assets/Scripts/SetToEdge.cs
@@ -2,8 +2,13 @@
 
 public class SetToEdge : MonoBehaviour {
 
+	public bool left;
+
 	void Update () 
 	{
-		transform.position = FindObjectOfType<Camera>().ScreenToWorldPoint(new Vector3(0.0f, Screen.height/2, 0.0f));
+		if(left)
+			transform.position = FindObjectOfType<Camera>().ScreenToWorldPoint(new Vector3(0.0f, Screen.height/2, 0.0f));
+		else
+			transform.position = FindObjectOfType<Camera>().ScreenToWorldPoint(new Vector3(Screen.width, Screen.height/2, 0.0f));
 	}
 }
