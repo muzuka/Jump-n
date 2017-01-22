63c556df152f301b05ad92f959bf8374a39f7e34
muzuka <seanbrown7935@gmail.com>
2017-01-21 19:43:05 -0700

    Controls Main Menu.

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour {
+
+	public bool debug;
+
+	public void startGame ()
+	{
+		EditorSceneManager.LoadScene("MainLevel");
+	}
+
+	public void quitGame ()
+	{
+		if(debug)
+			UnityEditor.EditorApplication.isPlaying = false;
+		else
+			Application.Quit();
+	}
+}
