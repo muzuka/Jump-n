7593afec8f2fa4aade748bb227062aa6253e6183
muzuka <seanbrown7935@gmail.com>
2017-01-21 19:42:15 -0700

    Now builds whole platform.

--- Assets/Scripts/LevelBuilder.cs
+++ Assets/Scripts/LevelBuilder.cs
@@ -10,6 +10,8 @@ public class LevelBuilder : MonoBehaviour {
 
 	public Vector2 moveRight;
 	public Vector2 moveUp;
+	public Vector2 addTop;
+	public Vector2 addEnd;
 
 	Vector2 current;
 
@@ -33,8 +35,23 @@ public class LevelBuilder : MonoBehaviour {
 		{
 			for(int h = 0; h < l.sequence[i]; h++)
 			{
-				Instantiate(Resources.Load("Prefabs/PlatformTile"), current, Quaternion.identity); 
+				float tile = Random.Range(0.0f, 3.0f);
+
+				if(tile <= 1.0f)
+					Instantiate(Resources.Load("Prefabs/PlatformTile"), current, Quaternion.identity); 
+				else if(tile <= 2.0f)
+					Instantiate(Resources.Load("Prefabs/PlatformTile2"), current, Quaternion.identity); 
+				else if(tile <= 3.0f)
+					Instantiate(Resources.Load("Prefabs/PlatformTile3"), current, Quaternion.identity); 
+				
 				current += moveUp;
+			}
+
+			if(l.sequence[i] != 0)
+			{
+				Instantiate(Resources.Load("Prefabs/PlatformTop"), current + addTop, Quaternion.identity);
+				Instantiate(Resources.Load("Prefabs/PlatformLeftEnd"), current + addTop - addEnd, Quaternion.identity);
+				Instantiate(Resources.Load("Prefabs/PlatformRightEnd"), current + addTop + addEnd, Quaternion.identity); 
 			}
 
 			current += moveRight;
